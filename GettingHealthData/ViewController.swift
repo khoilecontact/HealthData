47a1900b9cc044dad4122937c8bfe823fcbdd9bc
KhoiLe <Khoilecontact@gmail.com>
2022-06-10 09:19:19 +0700

    Initial Commit

---
+++ GettingHealthData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GettingHealthData
+//
+//  Created by KhoiLe on 10/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
